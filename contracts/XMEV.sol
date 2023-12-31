992e51464487af8bcc4856f53b09147ef65aa4a1
deanbred <deanbred@gmail.com>
2023-12-31 04:59:02 -0500

    ownership

--- contracts/XMEV.sol
+++ contracts/XMEV.sol
@@ -19,8 +19,13 @@ abstract contract Context {
   }
 }
 
-contract Ownable is Context {
+abstract contract Ownable is Context {
   address private _owner;
+
+  error OwnableUnauthorizedAccount(address account);
+
+  error OwnableInvalidOwner(address owner);
+
   event OwnershipTransferred(
     address indexed previousOwner,
     address indexed newOwner
@@ -32,18 +37,36 @@ contract Ownable is Context {
     emit OwnershipTransferred(address(0), msgSender);
   }
 
-  function owner() public view returns (address) {
-    return _owner;
-  }
-
   modifier onlyOwner() {
-    require(_owner == _msgSender(), "Ownable: caller is not the owner");
+    _checkOwner();
     _;
   }
 
+  function owner() public view virtual returns (address) {
+    return _owner;
+  }
+
+  function _checkOwner() internal view virtual {
+    if (owner() != _msgSender()) {
+      revert OwnableUnauthorizedAccount(_msgSender());
+    }
+  }
+
   function renounceOwnership() public virtual onlyOwner {
-    emit OwnershipTransferred(_owner, address(0));
-    _owner = address(0);
+    _transferOwnership(address(0));
+  }
+
+  function transferOwnership(address newOwner) public virtual onlyOwner {
+    if (newOwner == address(0)) {
+      revert OwnableInvalidOwner(address(0));
+    }
+    _transferOwnership(newOwner);
+  }
+
+  function _transferOwnership(address newOwner) internal virtual {
+    address oldOwner = _owner;
+    _owner = newOwner;
+    emit OwnershipTransferred(oldOwner, newOwner);
   }
 }
 
